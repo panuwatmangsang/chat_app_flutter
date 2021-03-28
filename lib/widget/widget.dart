be5aeb010edcd2fa2640928038463b125bf0ee83
panuwatmangsang <panuwatmangsang@gmail.com>
2021-03-28 09:53:49 +0700

    commit
    final project mobile app

---
+++ lib/widget/widget.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+Widget appBarMain(BuildContext context) {
+  return AppBar(
+    title: Image.asset(
+      "assets/images/onepiecePNG.png",
+      height: 50,
+      // width: 100,
+    ),
+    elevation: 0.0,
+    centerTitle: false,
+  );
+}
+
+InputDecoration textFieldInputDecoration(String hintText) {
+  return InputDecoration(
+      hintText: hintText,
+      hintStyle: TextStyle(color: Colors.black),
+      focusedBorder:
+          UnderlineInputBorder(borderSide: BorderSide(color: Colors.black)),
+      enabledBorder:
+          UnderlineInputBorder(borderSide: BorderSide(color: Colors.black)));
+}
+
+TextStyle simpleTextStyle() {
+  return TextStyle(color: Colors.black, fontSize: 16);
+}
+
+TextStyle biggerTextStyle() {
+  return TextStyle(color: Colors.black, fontSize: 17);
+}
